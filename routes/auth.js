86a73bf294c720515babb0eb1dafb7eabf5ccb6b
Your Name <you@example.com>
2022-10-10 13:17:08 +0200

    final

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require("../models/User");
 const { registerValidation, loginValidation } = require("../validation")
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 
